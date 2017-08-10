0da2c908a389ad22c816821b62946836036d9cc3
unknown <D22436@SZCKT.COM>
2017-08-10 18:33:26 +0800

    inflate  weather fragment

--- app/src/main/java/com/yu/coolweather/WeatherFragment.java
+++ app/src/main/java/com/yu/coolweather/WeatherFragment.java
@@ -1,10 +1,21 @@
 package com.yu.coolweather;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
 /**
  * Created by D22436 on 2017/8/10.
  */
 
 public class WeatherFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_choose_area, container, false);
+        return view;
+    }
 }
